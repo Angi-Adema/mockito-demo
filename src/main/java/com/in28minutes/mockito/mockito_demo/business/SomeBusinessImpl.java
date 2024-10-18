617d9575a7dc8588f4a228b301dda1782f799703
Angi-Adema <dr.adema@gmail.com>
2024-10-18 11:36:29 -0600

    created package com.in28minutes.mockito.mockito_demo.business with class SomeBusinessImpl.java in order to set up unit test using Mockito.

---
+++ src/main/java/com/in28minutes/mockito/mockito_demo/business/SomeBusinessImpl.java
@@ -0,0 +1,30 @@
+package com.in28minutes.mockito.mockito_demo.business;
+
+public class SomeBusinessImpl {
+	
+	// This is a common pattern in dependency injection, where external dependencies are provided to a class rather than the class creating those dependencies itself.
+	private DataService dataService;
+	
+	// Find and return the greatest (maximum) value from a dataset retrieved through the DataService.
+	public int findTheGreatestFromAllData() {
+		
+		// Return an array of integers (int[] data). Retrieves all available data to be processed.
+		int[] data = dataService.retrieveAllData();
+		
+		// greatestValue is initialized with the smallest possible integer value in Java that serves as a baseline for comparison. Values in dataset will be larger than this value.
+		int greatestValue = Integer.MIN_VALUE;
+		
+		// Iterate over the data using a for-each loop to check if each value is greater than the greatestValue and if so it updates the greatestValue to the current value.
+		for(int value : data) {
+			if(value > greatestValue)
+				greatestValue = value;
+		}
+		// Greatest value is found and returned.
+		return greatestValue;
+	}
+}
+
+//This class will depend on an interface in the data layer.
+interface DataService {
+	int[] retrieveAllData();
+}
